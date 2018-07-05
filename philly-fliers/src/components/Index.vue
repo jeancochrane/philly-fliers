d84c6fc9d5757753ee859398a9a4c652e78cf855
Jean Cochrane <jean@jeancochrane.com>
2018-07-05 17:37:32 -0400

    Finish setting up dev environment and load some initial components

---
+++ philly-fliers/src/components/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    <leaflet-map/>
+</template>
+
+<script>
+import LeafletMap from './Map';
+
+export default {
+    name: 'IndexView',
+    components: {
+        'leaflet-map': LeafletMap,
+    }
+}
+</script>
---
+++ philly-fliers/src/components/Map.vue
@@ -0,0 +1,42 @@
+<template>
+    <div
+         id="map"
+         style="height:500px"></div>
+</template>
+
+<script>
+import L from 'leaflet';
+
+export default {
+    name: 'LeafletMap',
+    data () {
+        return {
+            map: null,
+        };
+    },
+    mounted () {
+        this.initialize();
+    },
+    methods: {
+        initialize () {
+            // Initialize a map.
+            this.map = new L.map('map', {
+                center: [39.95, -75.16],
+                zoom: 12,
+                dragging: true,
+                touchZoom: true,
+                tap: true,
+                scrollWheelZoom: false
+            });
+
+            // Add tile layers from OSM.
+            let tiles = new L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> Contributors',
+                maxZoom: 18,
+            }).addTo(this.map);
+        }
+    }
+}
+
+
+</script>
