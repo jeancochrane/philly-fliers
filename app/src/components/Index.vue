920563c13a7bce06abe5baa30836704589bd81e5
Jean Cochrane <jean@jeancochrane.com>
2018-07-11 11:30:16 -0400

    Revert "Temporarily remove app components to make code review easier"
    This reverts commit b82ff0157eb205f6c5b3424dc0418eb4e3615d79.

--- app/src/components/Index.vue
+++ app/src/components/Index.vue
@@ -1,11 +1,22 @@
 <template>
     <div class="container-fluid">
-        <h1>Hello, World!</h1>
+        <fliers-navbar/>
+        <fliers-public/>
+        <fliers-footer/>
     </div>
 </template>
 
 <script>
+import FliersNavbar from './Navbar';
+import FliersPublic from './Public';
+import FliersFooter from './Footer';
+
 export default {
-    name: 'IndexView'
+    name: 'IndexView',
+    components: {
+        'fliers-navbar': FliersNavbar,
+        'fliers-public': FliersPublic,
+        'fliers-footer': FliersFooter,
+    }
 }
 </script>
---
+++ app/src/components/Map.vue
@@ -0,0 +1,57 @@
+<template>
+    <div
+        ref="map"
+        style="height:500px">
+    </div>
+</template>
+
+<script>
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+export default {
+    name: 'LeafletMap',
+    props: {
+        id: {
+            type: String,
+            default: 'map',
+        },
+    },
+    data() {
+        return {
+            map: null,
+        };
+    },
+    mounted() {
+        this.initMap();
+        this.initLayers();
+    },
+    methods: {
+        initMap() {
+            //
+            // Initialize a Leaflet map in this component.
+            //
+            this.map = new L.map(this.$refs.map, {
+                center: [39.95, -75.16],
+                zoom: 12,
+                dragging: true,
+                touchZoom: true,
+                tap: true,
+                scrollWheelZoom: false
+            });
+        },
+
+        initLayers() {
+            //
+            // Add tile layers from OSM to the map.
+            //
+            const streets = new L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> Contributors',
+                maxZoom: 18,
+            }).addTo(this.map);
+        },
+    }
+}
+
+
+</script>
