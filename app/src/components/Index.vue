b82ff0157eb205f6c5b3424dc0418eb4e3615d79
Jean Cochrane <jean@jeancochrane.com>
2018-07-11 11:29:25 -0400

    Temporarily remove app components to make code review easier

--- app/src/components/Index.vue
+++ app/src/components/Index.vue
@@ -1,22 +1,11 @@
 <template>
     <div class="container-fluid">
-        <fliers-navbar/>
-        <fliers-public/>
-        <fliers-footer/>
+        <h1>Hello, World!</h1>
     </div>
 </template>
 
 <script>
-import FliersNavbar from './Navbar';
-import FliersPublic from './Public';
-import FliersFooter from './Footer';
-
 export default {
-    name: 'IndexView',
-    components: {
-        'fliers-navbar': FliersNavbar,
-        'fliers-public': FliersPublic,
-        'fliers-footer': FliersFooter,
-    }
+    name: 'IndexView'
 }
 </script>
