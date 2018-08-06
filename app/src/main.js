8868f594729ec9bb3da2bcbcf7042bef89c73457
Jean Cochrane <jean@jeancochrane.com>
2018-08-06 13:42:39 -0400

    Import Bootstrap styles first so that the app can override them

--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+//
+// Import Bootstrap and Bootstrap styles first, so that Vue Loader will load
+// those stlyes first. For details, see:
+// https://github.com/vuejs/vue-loader/issues/140#issuecomment-371270232
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Vue from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faThumbtack, faFilter, faArrowRight, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
@@ -8,9 +15,6 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import App from './App';
 import router from './router';
 import store from './store';
-
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 // Register FontAwesome component.
