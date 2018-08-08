2bf05c227a192db26143fccf941bf0b3996cb897
Jean Cochrane <jean@jeancochrane.com>
2018-08-08 12:17:47 -0400

    Upgrade AngularJS to 1.7 and use new Promise syntax in AuthService

--- app/scripts/auth/auth-service.js
+++ app/scripts/auth/auth-service.js
@@ -28,12 +28,12 @@
         module.authenticate = function(auth, needsAdmin) {
             var dfd = $q.defer();
             $http.post(ASEConfig.api.hostname + '/api/auth/token/post/', auth)
-            $http.post(ASEConfig.api.hostname + '/api-token-auth/', auth)
-            .success(function(data, status) {
+            .then(function(response) {
                 var result = {
-                    status: status,
+                    status: response.status,
                     error: ''
                 };
+                var data = response.data;
 
                 if (data && data.user && data.token) {
                     $log.debug('sending user service user:');
@@ -94,7 +94,8 @@
                         }
                     }
                 });
-            }).error(function(data, status) {
+            }).catch(function(response) {
+                var data = response.data;
                 var error = _.values(data).join(' ');
                 if (data.username) {
                     error = 'Username field required.';
@@ -104,7 +105,7 @@
                 }
                 var result = {
                     isAuthenticated: false,
-                    status: status,
+                    status: response.status,
                     error: error
                 };
                 dfd.resolve(result);
